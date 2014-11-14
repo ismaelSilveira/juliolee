cf0b5d693e00e3cb1bfa7289064a0b8cdb3d5edd
Agustin <javieragustinfarias@gmail.com>
2014-11-14 19:48:22 -0300

    Cambio en comportamiento subirpala para tener en cuenta si detecta robot o pared

--- src/JulioLee.java
+++ src/JulioLee.java
@@ -9,35 +9,41 @@ import lejos.robotics.subsumption.Behavior;
 public class JulioLee {
 	private static SensorPort PUERTO_COMPASS = SensorPort.S2;
 	private static SensorPort PUERTO_SONAR_IZQ = SensorPort.S3;
-//	private static SensorPort PUERTO_SONAR_DER = SensorPort.S1;
+	private static SensorPort PUERTO_SONAR_DER = SensorPort.S1;
 	private static int DISTANCIA_PARED = 21;
+	private static int DISTANCIA_ARRIBA = 296;
+	private static int DISTANCIA_ARRIBA_ZM_PARED = 1303;
+	private static int DISTANCIA_ARRIBA_ZM = 1093;
 
 	public static void main(String[] args) {
 		// Inicializacion de sensores
 		UltrasonicSensor sonar_izq = new UltrasonicSensor(PUERTO_SONAR_IZQ);
 		CompassHTSensor compass = new CompassHTSensor(PUERTO_COMPASS);
 		compass.resetCartesianZero();
-		
+
 		Comunicacion com = new Comunicacion();
 		com.start();
-		
+
 		sensorDistancia distancia = new sensorDistancia(com);
 		distancia.start();
 
 		// Inicializacion de comportamientos
-		Behavior avanzar = new Avanzar(Motor.B, Motor.A, Motor.C, sonar_izq, DISTANCIA_PARED, com);
-		Behavior subir_pala = new SubirPala(Motor.B, Motor.A, Motor.C, sonar_izq, DISTANCIA_PARED, compass, com);
-		Behavior bajar_pala = new BajarPala(Motor.C, sonar_izq, DISTANCIA_PARED,com);
+		Behavior avanzar = new Avanzar(Motor.B, Motor.A);
+		Behavior subir_pala = new SubirPala(Motor.B, Motor.A, Motor.C,
+				sonar_izq, DISTANCIA_PARED, DISTANCIA_ARRIBA,
+				DISTANCIA_ARRIBA_ZM_PARED, compass, com, distancia, compass);
+		Behavior bajar_pala = new BajarPala(Motor.C, sonar_izq,
+				DISTANCIA_PARED, com);
 		Behavior girar = new Girar(Motor.B, Motor.A, Motor.C, compass, com);
-		Behavior sensarYPatear = new SensarYPatear(com, compass, Motor.B, Motor.A);
-		Behavior[] comportamientos = { avanzar, bajar_pala, girar, subir_pala, sensarYPatear };
-	/*	Motor.B.setSpeed(50);
-		Motor.A.setSpeed(50);
-		compass.startCalibration();
-		Motor.B.rotate(4560, true);
-		Motor.A.rotate(-4560);
-		compass.stopCalibration();
-*/
+		Behavior sensarYPatear = new SensarYPatear(com, compass, Motor.B,
+				Motor.A);
+		Behavior[] comportamientos = { avanzar, bajar_pala, girar, subir_pala,
+				sensarYPatear };
+		/*
+		 * Motor.B.setSpeed(50); Motor.A.setSpeed(50);
+		 * compass.startCalibration(); Motor.B.rotate(4560, true);
+		 * Motor.A.rotate(-4560); compass.stopCalibration();
+		 */
 		Arbitrator arbitro = new Arbitrator(comportamientos);
 		try {
 			Thread.sleep(3000);
