4b7cb79b7b82d57ab8643cdc20de4dbb05dda004
Victoria-Diaz <mavidiaz282@gmail.com>
2014-11-20 13:20:20 -0200

    me falto agregar el constante

---
+++ src/Constante.java
@@ -0,0 +1,16 @@
+import lejos.nxt.SensorPort;
+
+
+public class Constante {
+	public static SensorPort PUERTO_COMPASS = SensorPort.S2;
+	public static SensorPort PUERTO_SONAR_IZQ = SensorPort.S3;
+	public static SensorPort PUERTO_SONAR_DER = SensorPort.S1;
+	public static int DISTANCIA_PARED = 21;
+	public static int DISTANCIA_ARRIBA = 296;
+	public static int DISTANCIA_ARRIBA_ZM_PARED = 1300;
+	public static int DISTANCIA_ARRIBA_ZM = 1090;
+	public static int DISTANCIA_AGGREGATION_MAX = 40;
+	public static int DISTANCIA_AGGREGATION_MIN = 35;
+	public static int DISTANCIA_DISPERSION_MAX = 30;
+	public static int DIFERNECIA_ARRIBA_Y_ABAJO = 17;
+}
